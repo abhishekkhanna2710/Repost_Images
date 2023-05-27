b210d2a38f5003b4e966ba606f03435a2c604210
abhishekkhanna2710 <abhikhanna2710@gmail.com>
2023-05-27 12:03:41 +0530

    Schema Done

---
+++ server/models/imageSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const itemsSchema = new mongoose.Schema({
+    serialNumber:{
+        type:String,
+        required :true,
+    },
+    title:{
+        type:String,
+        requuired:true,
+    },
+    image_url:{
+        type:String,
+        required:true,
+    }
+})
+
+
+const ItemImage = mongoose.model('imagesList', itemsSchema);
+
+module.expors = ItemImage;
